a016e1756930dfbf8288eef8112ee41c74da8263
Sebas <sebastian.ituarte@gmail.com>
2024-05-28 16:15:04 -0400

    fixed validate middleware
    fixed validate middleware

--- src/infrastructure/middleware/validate.middleware.ts
+++ src/infrastructure/middleware/validate.middleware.ts
@@ -1,36 +1,24 @@
 // Module import
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { inject, injectable } from 'inversify';
 import { ObjectSchema } from 'joi';
-// Interface import
-import { ILogger, LOGGER_TYPE } from '../logging/logger.interface';
+// Domain import
+import { ErrorNames, ResponseErrorDomain } from '../../domains/error.domain';
 
-@injectable()
-class ValidationMiddleware {
-  constructor(@inject(LOGGER_TYPE.Logger) private _logger: ILogger) {}
-
-  // This middleware function will validate the request body using a joi schema
-  public validate(schema: ObjectSchema) {
-    return (req: Request, res: Response, next: NextFunction) => {
-      const { error } = schema.validate(req.body);
-      if (error) {
-        this._logger.error('Validation error', {
-          method: req.method,
-          url: req.url,
-          body: req.body,
-          params: req.params,
-          query: req.query,
-          validationError: error.details[0].message,
-        });
-
-        return res
-          .status(StatusCodes.BAD_REQUEST)
-          .json({ error: true, message: error.details[0].message });
-      }
-      return next();
-    };
-  }
+// This middleware function will validate the request body using a joi schema
+function validate(schema: ObjectSchema) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      throw new ResponseErrorDomain({
+        name: ErrorNames.BadRequestError,
+        error: true,
+        code: StatusCodes.BAD_REQUEST,
+        message: error.details[0].message,
+      });
+    }
+    return next();
+  };
 }
 
-export { ValidationMiddleware };
+export { validate };
